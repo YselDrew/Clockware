19e3825596e9776ad12949be6114ca1e3d0a1275
IselDrew <andryshonochek0604@gmail.com>
2020-07-10 13:58:43 +0300

    Update component with exceptions

--- src/modules/client/client.service.ts
+++ src/modules/client/client.service.ts
@@ -1,12 +1,17 @@
 import { Client } from './client.model';
+import { NotFound } from '../../common/exceptions'
 
 class ClientService {
     public async findMany(): Promise<Client[]> {
         return Client.findAll();
     }
 
-    public async findOneById(id): Promise<Client> {
-        return Client.findOne({ where: { id } });
+    public async findOneById(id: number): Promise<Client> {
+        const client: Client = await Client.findOne({ where: { id } });
+        if (!client) {
+            throw new NotFound(`There is no client with id ${id}`);
+        }
+        return client;
     }
 }
 
